f4180f2d0f6e011063da72d602b68231d12483a7
jwestgard <jwestgard@mac.com>
2016-04-22 17:09:45 -0400

    add config loader, argparsing, and testdata

--- restore.py
+++ restore.py
@@ -12,8 +12,25 @@ import yaml
 
 
 def main():
-    print('Hello World!')
+    '''Parse args, loop over repository and restore.'''
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description='Restore serialized Fedora repository.')
+    parser.add_argument('-c', '--config', required=True,
+        help='relative or absolute path to the YAML config file')
+    args = parser.parse_args()
     
+    # load and parse specified configuration settings
+    with open(args.config, 'r') as configfile:
+        globals().update(yaml.safe_load(configfile))
+
+    print('Ready to load to endpoint at: {0}'.format(REST_ENDPOINT))
+    print('Testing connection to server with provided credentials ...')
+    
+    resp = requests.get(REST_ENDPOINT, auth=(FEDORA_USER, FEDORA_PASSWORD))
+    print(resp)
+
 
 if __name__ == "__main__":
     main()
